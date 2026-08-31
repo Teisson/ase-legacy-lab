9de59337340b7389f5bf90d3c93ae537332afb07
Teisson <teisnilsson@gmail.com>
2026-08-31 09:44:15 +0200

    Transaction with recitified order_id

--- sql/05_transactions.sql
+++ sql/05_transactions.sql
@@ -1,3 +1,15 @@
+/*
+    Transaction handling demonstration.
+
+    Demonstrates atomic execution of multiple statements:
+    - successful statements are committed together
+    - an error causes the transaction to roll back
+    - RETURN prevents execution from continuing after rollback
+
+    This script uses fixed test IDs and is intended for demonstration,
+    not as an idempotent deployment script.
+*/
+
 BEGIN TRAN
 
 INSERT INTO purchase_orders (order_id, supplier_id, amount, order_date)
@@ -10,7 +22,7 @@ END
 
 
 INSERT INTO purchase_orders (order_id, supplier_id, amount, order_date)
-VALUES (2, 3, 25000, '2026-08-03 14:30:00')
+VALUES (7, 3, 25000, '2026-08-03 14:30:00')
 IF @@error != 0
 BEGIN
 ROLLBACK TRAN
