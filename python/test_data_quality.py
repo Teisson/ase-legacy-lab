d2b12b51a9a0f7de95e103a25053beb861f13059
Teisson <teisnilsson@gmail.com>
2026-09-24 13:33:26 +0200

    Refactor data quality tests to pytest

--- python/test_data_quality.py
+++ python/test_data_quality.py
@@ -1,11 +1,10 @@
-from unittest import TestCase
+import pytest
 from data_quality import check_suppliers, check_orders
 
-class TestDataQualityChecker(TestCase):
-    def test_empty_suppliers_list(self):
-        with self.assertRaises(ValueError):
-            check_suppliers([])
+def test_empty_suppliers_list():
+    with pytest.raises(ValueError):
+        check_suppliers([])
 
-    def test_empty_orders_list(self):
-        with self.assertRaises(ValueError):
+def test_empty_orders_list():
+    with pytest.raises(ValueError):
             check_orders([])
